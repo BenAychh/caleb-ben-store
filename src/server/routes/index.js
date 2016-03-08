4f8e65aa23bcaefe5bc13d9fb4a1ad24316c35ef
Caleb Pollman <cpollman1@gmail.com>
2016-03-07 17:14:13 -0700

    added index route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,21 @@
+var knex = require('../../../db/knex.js');
 var express = require('express');
 var router = express.Router();
 
+function Animals() {
+	return knex('inventory');
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	
+	Animals().select('*').orderBy('animal_name').then(function(result){
+			res.render('index', {
+				'title': 'Super Slow Pets',
+				'animals': result
+		});
+	});
 });
+
+
 
 module.exports = router;
